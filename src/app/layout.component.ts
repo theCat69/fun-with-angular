4af7a97fd5cd3f92f2cfe0b695d20263cf661cb5
Felix Vadcard <vadcard.felix@gmail.com>
2025-10-04 11:18:58 +0200

    hamburger

--- src/app/layout.component.ts
+++ src/app/layout.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule],
+  imports: [CommonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
@@ -22,4 +23,9 @@ export class LayoutComponent {
   ];
   // Provide a simple items array for the template to iterate over
   items = Array.from({ length: 30 }, (_, i) => i + 1);
+  @ViewChild('drawer') drawer?: MatSidenav;
+
+  toggleSidenav() {
+    this.drawer?.toggle();
+  }
 }
