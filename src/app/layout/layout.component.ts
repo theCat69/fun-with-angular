bcb710ec773ff41e33492d2680e914228ff55df0
Felix Vadcard <vadcard.felix@gmail.com>
2025-10-05 08:21:26 +0200

    moved theming prebuild to angular.json. Mat select with flags and ngOptimizedImage

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
@@ -7,11 +7,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CONFIG } from '../../main';
 import { Config } from '../../models/config';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, NgOptimizedImage, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
@@ -30,6 +32,12 @@ export class LayoutComponent {
   config: Config = inject(CONFIG);
 
   connected: boolean = true;
+
+  countries = [
+    { code: 'fr', label: 'French', img: 'france-flag.webp' },
+    { code: 'de', label: 'German', img: 'germany-flag.webp' },
+    { code: 'it', label: 'Italian', img: 'italy-flag.webp' }
+  ]
 
   toggleSidenav() {
     this.drawer?.toggle();
