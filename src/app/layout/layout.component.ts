bc3f0607d2e5a8045e3e31e4db2d466477fe3992
Felix Vadcard <vadcard.felix@gmail.com>
2025-10-09 07:49:24 +0200

    fun again

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
@@ -6,7 +6,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelect, MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { Theme, ThemeService } from '../services/theme.service';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AppRoute, routes } from '../app.routes';
@@ -19,17 +19,19 @@ import { AppRoute, routes } from '../app.routes';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-  @ViewChild('drawer') 
+  @ViewChild('drawer')
   drawer?: MatSidenav;
+
+  selectedCountry = signal({} as { code: string; label: string; img: string });
 
   connected: boolean = true;
 
   appRoutes: AppRoute[] = routes;
 
   countries = [
-    { code: 'fr', label: 'French', img: 'france-flag.webp' },
-    { code: 'de', label: 'German', img: 'germany-flag.webp' },
-    { code: 'it', label: 'Italian', img: 'italy-flag.webp' }
+    { code: 'fr', label: 'French', img: 'france-flag' },
+    { code: 'de', label: 'German', img: 'germany-flag' },
+    { code: 'it', label: 'Italian', img: 'italy-flag' }
   ]
 
   private readonly themeService = inject(ThemeService);
@@ -47,4 +49,10 @@ export class LayoutComponent implements OnInit {
   toggleSidenav() {
     this.drawer?.toggle();
   }
+
+  languageChanged(value: any) {
+    const selectedLang = this.countries.find(c => c.code === value);
+    this.selectedCountry.set(selectedLang ? selectedLang: {} as { code: string; label: string; img: string; }); 
+  }
+
 }
